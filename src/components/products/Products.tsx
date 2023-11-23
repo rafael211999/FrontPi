829e22880483c55bba19ec594fb2ac6334f09285
PROGRAMADOR JAVA  11612841.2023.1 <116128412023.1@senacrjedu.local>
2023-11-22 21:50:35 -0300

    conectando o banco

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -41,10 +41,6 @@ function Products() {
 
 
 
-
-
-
-
   return (
     <>
       <Navibar />
--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,7 +1,48 @@
 import React from "react";
 import Navibar from "../navibar/Navibar";
+import axios from "axios";
 
 function User() {
+
+  async function userDataPost() {
+    // Montando o objeto de dados JSON
+
+    var name = (document.getElementById("sName") as HTMLInputElement) || null;
+    var lastName = (document.getElementById("sSobrenome") as HTMLInputElement) || null;
+    var email = (document.getElementById("sEmail") as HTMLInputElement) || null;
+    var cpf = (document.getElementById("sCpf") as HTMLInputElement) || null;
+    
+
+
+    const userJson = {
+      name: name.value.toString(),
+      last_name: lastName.value.toString(),
+      email: email.value.toString(),
+      cpf: cpf.value.toString(),
+    };
+  
+    await axios
+      .post("http://localhost:9000/api/users", userJson, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        maxRedirects: 0,
+      })
+      .then((response) => {
+        console.log(response);
+        alert("Usuário cadastrado com sucesso")
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
+
+
+
+
   return (
     <>
      <Navibar />
@@ -13,6 +54,7 @@ function User() {
             type="text"
             placeholder="Nome"
             rel="noopener noreferrer"
+            id="sName"
           />
 
           <input
@@ -20,6 +62,7 @@ function User() {
             type="text"
             placeholder="Sobrenome"
             rel="noopener noreferrer"
+            id="sSobrenome"
           />
 
           <input
@@ -27,6 +70,7 @@ function User() {
             type="text"
             placeholder="Email"
             rel="noopener noreferrer"
+            id="sEmail"
           />
 
           <input
@@ -34,10 +78,11 @@ function User() {
             type="text"
             placeholder="Cpf"
             rel="noopener noreferrer"
+            id="sCpf"
           />
         </div>
 
-        <button id="cadastrarProduto" className="MyButton"> Cadastrar </button>
+        <button id="cadastrarProduto" className="MyButton" onClick={userDataPost}> Cadastrar </button>
       </div>
     </>
   );
